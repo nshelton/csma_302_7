e4abe5a02d7ac3251f764d543617b43cc74f988e
h! <harrison.coutee@gmail.com>
2021-12-01 10:11:10 -0800

    Revert "Almost done (just need to fix lighting issues)"
    This reverts commit b51404628c27eb678c2fafdba0d95394777ebc1d.

--- Assets/RayTracingMaster.cs
+++ Assets/RayTracingMaster.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using System.Collections.Generic;
-using System.Linq;
 
 public class RayTracingMaster : MonoBehaviour
 {
@@ -8,45 +7,17 @@ public class RayTracingMaster : MonoBehaviour
     public Texture Skybox;
     public float SkyBrightness;
 
-    // public int NumSpheres;
-    // public float SpherePlacementRadius;
+    public int NumSpheres;
+    public float SpherePlacementRadius;
     public Vector2 SphereRadius;
-
-    public Color albedo = new Color(0.5f, 0.5f, 0.5f);
-    [Range(0,1)] public float smoothness = 0.5f;
-    [Range(0,1)] public float specular = 0.5f;
-    public Color emission = new Color(0.5f, 0.5f, 0.5f);
-
-    [Range(1,12)] public int bounces = 8;
 
     private RenderTexture _target;
     private RenderTexture _converged;
     
     private uint _currentSample = 0;
     private Material _addMaterial;
-    private static List<RayTracingObject> _rayTracingObjects = new List<RayTracingObject>();
     private ComputeBuffer _sphereBuffer;
 
-    struct MeshObject
-    {
-        public Matrix4x4 localToWorldMatrix;
-        public int indices_offset;
-        public int indices_count;
-
-        public Vector3 albedo;
-        public float smoothness;
-        public float specular;
-        public Vector3 emission;
-    }
-
-    private static List<MeshObject> _meshObjects = new List<MeshObject>();
-    private static List<MeshObject> _oldMeshObjects = new List<MeshObject>();
-    private static List<Vector3> _vertices = new List<Vector3>();
-    private static List<int> _indices = new List<int>();
-
-    private ComputeBuffer _meshObjectBuffer;
-    private ComputeBuffer _vertexBuffer;
-    private ComputeBuffer _indexBuffer;
     private int SPHERE_STRIDE = 56;
     // 56 bytes total
     struct Sphere {
@@ -54,72 +25,22 @@ public class RayTracingMaster : MonoBehaviour
         public float radius; 
         public Vector3 albedo;
         public Vector3 specular;
-        public Vector3  emission;
+        public Vector3 emission;
         public float smoothness;
-    }
-
-    private static bool _meshObjectsNeedRebuilding = false;
-    public static void RegisterObject(RayTracingObject obj)
-    {
-        _rayTracingObjects.Add(obj);
-        _meshObjectsNeedRebuilding = true;
-    }
-    public static void UnregisterObject(RayTracingObject obj)
-    {
-        _rayTracingObjects.Remove(obj);
-        _meshObjectsNeedRebuilding = true;
     }
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        RebuildMeshObjectBuffers();
         Render(destination);
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (transform.hasChanged || DirectionalLight.transform.hasChanged)
         {
-            _meshObjectsNeedRebuilding = true;
-        }
-
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            var resWidth = 1920;
-            var resHeight = 1080;
-
-            RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
-            GetComponent<Camera>().targetTexture = rt;
-            Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
-            GetComponent<Camera>().Render();
-            RenderTexture.active = rt;
-            screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
-            GetComponent<Camera>().targetTexture = null;
-            RenderTexture.active = null; // JC: added to avoid errors
-            Destroy(rt);
-            byte[] bytes = screenShot.EncodeToPNG();
-            string filename = "screenshot" + Time.time.ToString("F2") + ".png";
-            System.IO.File.WriteAllBytes(filename, bytes);
-            Debug.Log(string.Format("Took screenshot to: {0}", filename));
-        }
-
-        // check if each _meshObject has changed
-        for (int i = 0; i < _meshObjects.Count; i++)
-        {
-            var mesh = _meshObjects[i];
-            mesh.albedo = new Vector3(albedo.r, albedo.g, albedo.b);
-            mesh.smoothness = smoothness;
-            mesh.specular = specular;
-            mesh.emission = new Vector3(emission.r, emission.g, emission.b);
-
-            if (_oldMeshObjects[i].albedo != mesh.albedo ||
-                _oldMeshObjects[i].smoothness != mesh.smoothness ||
-                _oldMeshObjects[i].specular != mesh.specular ||
-                _oldMeshObjects[i].emission != mesh.emission)
-            {
-                _meshObjectsNeedRebuilding = true;
-                break;
-            }
+            _currentSample = 0;
+            transform.hasChanged = false;
+            DirectionalLight.transform.hasChanged = false;
         }
     }
 
@@ -135,19 +56,11 @@ public class RayTracingMaster : MonoBehaviour
             RayTracingShader.SetBuffer(0, "_Spheres", _sphereBuffer);
         }
 
-        SetComputeBuffer("_MeshObjects", _meshObjectBuffer);
-        SetComputeBuffer("_Vertices", _vertexBuffer);
-        SetComputeBuffer("_Indices", _indexBuffer);
-
-        SetComputeBuffer("_Indices", _indexBuffer);
-
         RayTracingShader.SetMatrix("_CameraToWorld", Camera.main.cameraToWorldMatrix);
         RayTracingShader.SetMatrix("_CameraInverseProjection", Camera.main.projectionMatrix.inverse);
         RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
         RayTracingShader.SetFloat("_Seed", Random.value);
         RayTracingShader.SetFloat("_SkyBrightness", SkyBrightness);
-
-        RayTracingShader.SetInt("_Bounces", bounces);
 
         int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
         int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
@@ -197,117 +110,50 @@ public class RayTracingMaster : MonoBehaviour
     }
     private void OnDisable()
     {
-        if (_vertexBuffer != null)
-            _vertexBuffer.Release();
-        if (_indexBuffer != null)
-            _indexBuffer.Release();
-        if (_meshObjectBuffer != null)
-            _meshObjectBuffer.Release();
+        if (_sphereBuffer != null)
+            _sphereBuffer.Release();
     }
     private void SetUpScene()
     {
-
-    }
-
-    private void RebuildMeshObjectBuffers()
-    {
-        if (!_meshObjectsNeedRebuilding)
-        {
-            return;
-        }
-        _meshObjectsNeedRebuilding = false;
-        _currentSample = 0;
-        // Clear all lists
-        _meshObjects.Clear();
-        _vertices.Clear();
-        _indices.Clear();
-
         List<Sphere> spheres = new List<Sphere>();
 
-        // Loop over all objects and gather their data
-        foreach (RayTracingObject obj in _rayTracingObjects)
+
+        // Add a number of random spheres
+        for (int i = 0; i < NumSpheres; i++)
         {
-            if (obj.isLight == false) {
-                Mesh mesh = obj.GetComponent<MeshFilter>().sharedMesh;
-                // Add vertex data
-                int firstVertex = _vertices.Count;
-                _vertices.AddRange(mesh.vertices);
-                // Add index data - if the vertex buffer wasn't empty before, the
-                // indices need to be offset
-                int firstIndex = _indices.Count;
-                var indices = mesh.GetIndices(0);
-                _indices.AddRange(indices.Select(index => index + firstVertex));
-                
-                // Add the object itself
-                _meshObjects.Add(new MeshObject()
-                {
-                    localToWorldMatrix = obj.transform.localToWorldMatrix,
-                    indices_offset = firstIndex,
-                    indices_count = indices.Length,
-                    albedo = new Vector3(albedo.r, albedo.g, albedo.b),
-                    specular = specular,
-                    emission = new Vector3(emission.r, emission.g, emission.b),
-                    smoothness = smoothness,
-                });
-            } else {
-                RayTracingObject light = obj;
-                Sphere sphere = new Sphere();
-                // Radius and radius
-                Vector2 randomPos = Random.insideUnitCircle * 50;
-                sphere.radius = SphereRadius.x + 1 * (SphereRadius.y - SphereRadius.x);
-                sphere.position = new Vector3(light.transform.position.x, light.transform.position.y, light.transform.position.z);
-        
-                sphere.albedo = new Vector3(obj.lightColor.r, obj.lightColor.g, obj.lightColor.b) * obj.intensity;
-                sphere.specular = Vector3.zero;
-                sphere.emission = new Vector3(obj.lightColor.r, obj.lightColor.g, obj.lightColor.b) * obj.intensity;
-                
-                sphere.smoothness = 1;
-                
-                // Add the sphere to the list
-                spheres.Add(sphere);
-            }
-        }
-
-        _oldMeshObjects = _meshObjects;
-
-        CreateComputeBuffer(ref _meshObjectBuffer, _meshObjects, 104);
-        CreateComputeBuffer(ref _vertexBuffer, _vertices, 12);
-        CreateComputeBuffer(ref _indexBuffer, _indices, 4);
-        CreateComputeBuffer(ref _sphereBuffer, spheres, 56);
-    }
-
-    private static void CreateComputeBuffer<T>(ref ComputeBuffer buffer, List<T> data, int stride)
-        where T : struct
-    {
-        // Do we already have a compute buffer?
-        if (buffer != null)
-        {
-            // If no data or buffer doesn't match the given criteria, release it
-            if (data.Count == 0 || buffer.count != data.Count || buffer.stride != stride)
+            Sphere sphere = new Sphere();
+            // Radius and radius
+            sphere.radius = SphereRadius.x + Random.value * (SphereRadius.y - SphereRadius.x);
+            Vector2 randomPos = Random.insideUnitCircle * SpherePlacementRadius;
+            sphere.position = new Vector3(randomPos.x, sphere.radius, randomPos.y);
+            // Reject spheres that are intersecting others
+            foreach (Sphere other in spheres)
             {
-                buffer.Release();
-                buffer = null;
+                float minDist = sphere.radius + other.radius;
+                if (Vector3.SqrMagnitude(sphere.position - other.position) < minDist * minDist)
+                    goto SkipSphere;
             }
-        }
-        if (data.Count != 0)
-        {
-            // If the buffer has been released or wasn't there to
-            // begin with, create it
-            if (buffer == null)
-            {
-                buffer = new ComputeBuffer(data.Count, stride);
+
+            // Albedo and specular color
+            Color color = Random.ColorHSV();
+            bool metal = Random.value < 0.3f;
+            sphere.albedo = metal ? Vector3.zero : new Vector3(color.r, color.g, color.b);
+            sphere.specular = metal ? new Vector3(color.r, color.g, color.b) : Vector3.one * 0.04f;
+
+            bool emissive = Random.value < 0.3f;
+            if (emissive) {
+                sphere.emission = new Vector3(color.r, color.g, color.b) * 3f;
             }
-            // Set data on the buffer
-            buffer.SetData(data);
+            
+            sphere.smoothness = Random.value;
+            
+            // Add the sphere to the list
+            spheres.Add(sphere);
+        SkipSphere:
+            continue;
         }
-    }
-    private void SetComputeBuffer(string name, ComputeBuffer buffer)
-    {
-        if (buffer != null)
-        {
-            RayTracingShader.SetBuffer(0, name, buffer);
-        } else {
-            Debug.LogError("Buf with name "+name+" is null");
-        }
+        // Assign to compute buffer
+        _sphereBuffer = new ComputeBuffer(spheres.Count, SPHERE_STRIDE);
+        _sphereBuffer.SetData(spheres);
     }
 }
