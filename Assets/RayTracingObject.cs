b51404628c27eb678c2fafdba0d95394777ebc1d
h! <harrison.coutee@gmail.com>
2021-12-01 09:04:10 -0800

    Almost done (just need to fix lighting issues)

---
+++ Assets/RayTracingObject.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class RayTracingObject : MonoBehaviour
+{
+    public bool isLight = false;
+    public Color lightColor = Color.white;
+    public float intensity = 5;
+
+    private void OnEnable()
+    {
+        RayTracingMaster.RegisterObject(this);
+    }
+    private void OnDisable()
+    {
+        RayTracingMaster.UnregisterObject(this);
+    }
+}
